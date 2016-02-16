b595cb068c050eef2f9ab6b0036c527ef2d60059
Janos Strasszer <little.evil.froggy@gmail.com>
2016-02-16 14:23:48 +0100

    Code and result files added

---
+++ plot1.R
@@ -0,0 +1,14 @@
+
+# Load and transform data
+raw_data <- read.csv("household_power_consumption.txt", sep = ";", na.strings = "?")
+data <- subset(raw_data, Date == "1/2/2007" | Date == "2/2/2007")
+rm(raw_data)
+data$DateTime <- paste(data$Date, data$Time, sep=" ") # Concatanation of date and time columns
+data <- data[, c(10,3,4,5,6,7,8,9)] # remove old date and time columns, move DateTime to the 1st place
+data$DateTime <- as.POSIXct(data$DateTime, format="%d/%m/%Y %H:%M:%S") # convert to POSIX calendar time
+
+
+# Plot1
+png(filename = "plot1.png", width = 480, height = 480)
+hist(data$Global_active_power, main = "Global Active Power", col = "Red", xlab = "Global Active Power (kilowatts)")
+dev.off()
